1ebdfd6bb6270e34b54257a46a675862ce8d6702
Colin Douglas <colindouglas@gmail.com>
2018-01-22 21:46:57 -0400

    Generalizing to all teams

--- optimize-keeper.R
+++ optimize-keeper.R
@@ -1,37 +1,57 @@
 library(adagio)
 library(tidyverse)
 
-league_min <- 550000
+league_min <- 535000
+salary_cap <- 75000000
+
+# Read in Data ------------------------------------------------------------
 
 salary_data <- read_csv("data/usatoday_salary.csv") %>%
-  filter(year == 2017) 
+  filter(year == 2017)  %>%
+  mutate(name = gsub("\\.","", name)) %>%
+  mutate(name = iconv(as.character(name), from="UTF-8", to="ASCII//TRANSLIT"))
   
 steamer_proj <- read_csv("data/steamer_2018_batters.csv") %>%
   select(Name, Team, WAR, playerid) %>%
-  bind_rows(read_csv("data/steamer_2018_pitchers.csv") %>% select(Name, Team, WAR, playerid))
-
-### TODO: Write some sort of lookup to fix this join
-not_in_steamer <- anti_join(salary_data, steamer_proj, by = c("name" =  "Name"))
-
-all_data <- inner_join(salary_data, steamer_proj, by = c("name" = "Name"))
-
-set.seed(69420)
-
-test_set <- all_data %>%
-  filter(WAR > 1) %>%
-  filter(row_number() %in% sample(n(), 20)) %>%
-  mutate(salary = salary / 10^6) %>%
-  arrange(WAR)
-
-result <- knapsack(test_set$salary, test_set$WAR, cap = 75) # Satisfy salary cap
-
-result <- knapsack(rep(1,20), test_set$WAR, cap = 4) # Satisfy roster slots
-
-print(test_set[result$indices,])
-paste("WAR:", sum(test_set$WAR[result$indices]))
-paste("Salary:", sum(test_set$salary[result$indices]))
+  bind_rows(read_csv("data/steamer_2018_pitchers.csv") %>% select(Name, Team, WAR, playerid)) %>%
+  mutate(Name = gsub("\\.","", Name)) %>%
+  mutate(Name = iconv(as.character(Name), from="UTF-8", to="ASCII//TRANSLIT"))
 
 
+all_rosters <- data_frame()
+
+for (i in 1:12) {
+  keeper_roster <- read_csv("data/DFL_rosters.csv") %>%
+    filter(fantasy_team == i) %>%
+    mutate(name = gsub("\\.","", name)) %>%
+    mutate(name = iconv(as.character(name), from="UTF-8", to="ASCII//TRANSLIT"))
+  
+  merged_keeper <- left_join(keeper_roster, steamer_proj, by = c("name" = "Name")) %>%
+    left_join(salary_data, by = c("name" = "name"))  %>%
+    mutate(salary = case_when(
+      is.na(salary)  ~ league_min / 10^5,
+      !is.na(salary) ~ salary / 10^5)) %>%
+    mutate(WAR = case_when(
+      is.na(WAR)  ~ 0,
+      !is.na(WAR) ~ WAR))
+  
+  
+  keepers <- knapsack(merged_keeper$salary, merged_keeper$WAR, cap = 750) # Satisfy salary cap
+  
+  best_roster <- data_frame(name = merged_keeper$name[keepers$indices],
+                            salary = merged_keeper$salary[keepers$indices] * 10^5,
+                            WAR = merged_keeper$WAR[keepers$indices],
+                            fantasy_team = i)
+  
+  all_rosters <- bind_rows(all_rosters, best_roster)
+}
+
+team_summary <- all_rosters %>%
+  group_by(fantasy_team) %>%
+  summarize(WAR = sum(WAR), salary = sum(salary))
+
+write_csv(all_rosters, "data/DFL_rosters_opt.csv")
+write_csv(team_summary, "data/DFL_rosters_opt_summary.csv")
 
 
 ##### Find the expected value of a draft pick at each slot
---
+++ scrape-yahoo-team.R
@@ -0,0 +1,64 @@
+### Use the httr bugfix from https://github.com/r-lib/httr/issues/493
+devtools::install_github("ctrombley/httr")
+library(httr)
+library(xml2)
+library(tidyverse)
+
+keys <- read_csv("../oauth_keys.csv") %>%
+  filter(website == "yahoo")
+
+userAgent <- "contact colindouglas@gmail.com"
+
+endpoint <- oauth_endpoint(
+  authorize = "https://api.login.yahoo.com/oauth2/request_auth",
+  access =    "https://api.login.yahoo.com/oauth2/get_token",
+  base_url = "https://api.login.yahoo.com/oauth2"
+)
+
+app <- oauth_app("Fantasy Baseball Scraper", keys$key, keys$secret, redirect_uri = "oob")
+
+token <- oauth2.0_token(endpoint = endpoint, app = app, use_oob = TRUE, config_init = user_agent(userAgent))
+
+### MLB 2017 GameID = 370
+leagueID <- "370.l.55086"
+
+# url <- paste0("https://fantasysports.yahooapis.com/fantasy/v2/team/", leagueID, ".t.", team, "?format=json")
+# team_info <- GET(url, user_agent(userAgent), config(token = token)) %>% content() 
+
+fantasy_teams <- data_frame()
+for (i in 1:12) {
+  ### Wait two seconds so the API doesn't get mad
+  Sys.sleep(2)
+  
+  team_number <- i
+
+  # Construct the Yahoo API call to get the team roster
+  url <- paste0("https://fantasysports.yahooapis.com/fantasy/v2/team/", leagueID, ".t.", team_number, "/roster?format=json")
+  roster_list <- GET(url, user_agent(userAgent), config(token = token)) %>% content()
+  
+  # Extract the player info out of the JSON response
+  roster <- data_frame(
+    player_key = unlist(map_if(roster_list$fantasy_content$team[[2]]$roster[[4]]$players, is.list, ~ .$player[[1]][[1]]$player_key)),
+    player_id = unlist(map_if(roster_list$fantasy_content$team[[2]]$roster[[4]]$players, is.list, ~ .$player[[1]][[2]]$player_id)),
+    name = unlist(map_if(roster_list$fantasy_content$team[[2]]$roster[[4]]$players, is.list, ~ .$player[[1]][[3]]$name$full)),
+    fantasy_team = team_number
+  ) %>% 
+    # Remove the last row of the df (its junk)
+    head(-1) %>% 
+    
+    # Transliterate the names so accents don't mess up your merges
+    mutate(name = iconv(as.character(name), from="UTF-8", to="ASCII//TRANSLIT"))
+  
+  fantasy_teams <- bind_rows(fantasy_teams, roster)
+
+}
+
+# Save the rosters as CSVs
+path <- paste0("data/DFL_rosters.csv")
+write_csv(fantasy_teams, path)
+
+
+# Gotta figure out some way of getting this vector into the dataframe  
+# positions <- map_if(roster$fantasy_content$team[[2]]$roster[[4]]$players, is.list, ~ .$player[[1]][[13]][[1]]) %>%
+#   map_if(is.list, ~unlist(.)) %>% bind_cols(team, .)
+
