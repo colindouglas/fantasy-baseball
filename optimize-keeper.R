54a5417b73547879afa8f0bdf955b4aa3702a85b
Colin Douglas <colindouglas@gmail.com>
2018-01-24 23:52:45 -0400

    Optimize keepers for expected draft value

--- optimize-keeper.R
+++ optimize-keeper.R
@@ -15,6 +15,7 @@ get_picks <- function(position, teams, slots) {
 league_min <- 535000
 salary_cap <- 75000000
 team_count <- 12
+roster_size <- 30
 draft_order <- order(c(11, 6, 1, 9, 2, 7, 5, 10, 4, 8, 3, 12)) # T11 picks first, T6 picks second, etc.
 
 # Read in Data ------------------------------------------------------------
@@ -22,6 +23,7 @@ draft_order <- order(c(11, 6, 1, 9, 2, 7, 5, 10, 4, 8, 3, 12)) # T11 picks first
 ### Import the USA Today Salary data set 
 salary_data <- read_csv("data/usatoday_salary.csv") %>%
   filter(year == 2017)  %>%
+  select(-aav) %>%
   
   ### Remove periods from names
   mutate(name = gsub("\\.","", name)) %>%
@@ -40,7 +42,7 @@ steamer_proj <- read_csv("data/steamer_2018_batters.csv") %>%
 # First pass of keeper optimization ---------------------------------------
 
 # Init the data frame
-all_keepers <- data_frame()
+keepers_max <- data_frame()
 
 for (i in 1:team_count) {
   
@@ -74,70 +76,97 @@ for (i in 1:team_count) {
     filter(row_number() %in% keepers$indices)
   
   ### Add it to the overall dataframe
-  all_keepers <- bind_rows(all_keepers, best_roster)
+  keepers_max <- bind_rows(keepers_max, best_roster)
   rm(keeper_roster, merged_keeper, best_roster)
 }
 
 ### Make a summary dataframe for each team
-team_summary <- all_keepers %>%
+team_summary <- keepers_max %>%
   group_by(fantasy_team) %>%
   summarize(keepers = n(), WAR = sum(WAR), salary = sum(salary)) %>%
   mutate(picks = 30 - keepers)
 
 ### Write the data to CSVs
-write_csv(all_keepers, "data/DFL_rosters_opt.csv")
-write_csv(team_summary, "data/DFL_rosters_opt_summary.csv")
+write_csv(keepers_max, "data/DFL_keepers_max.csv")
 
 # Attempt to value draft slots --------------------------------------------
 
-### Find the draftable players based on keeper optimization
-draftable <- steamer_proj %>%
+player_stats <- steamer_proj %>%
   left_join(salary_data, by = c("Name" = "name")) %>%
   mutate(salary = case_when(
     is.na(salary)  ~ league_min,
     !is.na(salary) ~ salary)) %>%
-  anti_join(all_keepers, by = c("Name" = "name")) %>%
-  filter(WAR > 0.2) %>%
-  arrange(desc(WAR))
+  mutate(WAR = case_when(
+    is.na(WAR)  ~ 0,
+    !is.na(WAR) ~ WAR))
 
-### Try to predict the WAR value of each draft slot. Wiggle is the width of the rolling average
-draft_wiggle <- 5
-draft_values <- draftable %>%
-  mutate(pick = row_number(), 
-         est_WAR = c(zoo::rollmean(x = WAR, draft_wiggle), rep(0, draft_wiggle-1))) %>%
-  select(pick, est_WAR)
+keepers_current <- keepers_max
+teams_trimmed <- 1
 
-###
-
-all_keepers_r2 <- data_frame()
-
-for (i in 1:team_count) {
-  team_number <- i
-
-  ### Load the list of keepers
-team_keepers <- all_keepers %>%
-  filter(fantasy_team == team_number)
-
-### 
-team_pick_count <- team_summary %>%
-  filter(fantasy_team == team_number) %>%
-  pull(picks)
-
-last_pick <- max(get_picks(draft_order[i], team_count, team_pick_count))
-
-last_pick_value <- draft_values %>%
-  filter(row_number() == last_pick) %>%
-  pull(est_WAR)
-
-all_keepers_r2 <- team_keepers %>%
-  filter(WAR > last_pick_value) %>%
-  bind_rows(all_keepers_r2, .)
-
+### Here is where the while loop needs to start
+while (teams_trimmed > 0) {
+  ### Find the draftable players based on keeper optimization
+  draftable_players <- player_stats %>%
+    anti_join(keepers_current, by = c("Name" = "name")) %>%
+    arrange(desc(WAR))
+  
+  ### Try to predict the WAR value of each draft slot. Wiggle is the width of the rolling average
+  draft_wiggle <- 5
+  slot_values <- draftable_players %>%
+    mutate(pick = row_number(), 
+           est_WAR = c(zoo::rollmean(x = WAR, draft_wiggle), rep(0, draft_wiggle-1))) %>%
+    select(pick, WAR, est_WAR)
+  
+  keepers_adj <- data_frame()
+  
+  ### Do this better: Stepwise, remove one pick at a time, throw it back into the pool.
+  teams_trimmed <- 0
+  for (i in 1:team_count) {
+    team_number <- i
+    ### Load the list of keepers from the last iteraton
+    keepers_team <- keepers_current %>%
+      filter(fantasy_team == team_number)
+    
+    ### Find out how many picks the team has
+    number_picks <- roster_size - nrow(keepers_team)
+    
+    ### Estimate the position of their last pick (very rough)
+    last_pick <- max(get_picks(draft_order[i], team_count, number_picks))
+    
+    ### Estimate the value of their last pick (very rough)
+    last_pick_value <- slot_values %>%
+      filter(row_number() == last_pick) %>%
+      pull(est_WAR)
+    
+    ### If the value of their last pick is more than one of their keepers, drop the least valuable keeper
+    if (last_pick_value > min(keepers_team$WAR)) {
+      keepers_team <- keepers_team %>%
+        arrange(desc(WAR)) %>%
+        head(-1) 
+      teams_trimmed <- teams_trimmed + 1
+    } else {
+      keepers_team <- keepers_team %>%
+        arrange(desc(WAR))
+    }
+    keepers_adj <- bind_rows(keepers_adj, keepers_team)
+    
+  }
+  keepers_current <- keepers_adj
 }
+rm(keepers_current)
 
-team_summary_r2 <- all_keepers_r2 %>%
+
+### Make a summary table with revisions
+team_summary <- keepers_max %>%
   group_by(fantasy_team) %>%
-  summarize(keepers_r2 = n(), WAR_r2 = sum(WAR), salary_r2 = sum(salary)) %>%
-  mutate(picks_r2 = 30 - keepers_r2) %>%
-  bind_cols(team_summary, .) %>%
-  mutate(diff_r2 = keepers - keepers_r2)
+  summarize(keepers_max = n(), WAR_max = sum(WAR), salary_max = sum(salary)*10^5) %>%
+  full_join(keepers_adj %>% 
+              group_by(fantasy_team) %>%
+              summarize(keepers_adj = n(), WAR_adj = sum(WAR), salary_adj = sum(salary)*10^5), 
+            by = "fantasy_team"
+  )
+
+
+
+write_csv(keepers_adj, "data/DFL_keepers_opt.csv")
+write_csv(team_summary, "data/DFL_keepers_summary.csv")
