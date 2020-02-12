bf22f3759da19eeb4d5b5825f80a6bfad8f5edaa
colin <colindouglas@gmail.com>
2020-02-11 23:33:40 -0400

    Cleanup of optimization script

--- optimize-keeper.R
+++ optimize-keeper.R
@@ -1,15 +1,6 @@
 library(tidyverse)
 
-# Functions ---------------------------------------------------------------
-
-### Function to get the draft positions of a team that drafts <position>th, 
-### in a league with x teams, and gets x draft slots overall
-get_picks <- function(position, teams, slots) {
-  return(seq(from = position, by = teams, length.out = slots))
-}
-
 # Constants ---------------------------------------------------------------
-
 league_min <- 555000
 salary_cap <- 68853720 # Not updated for 2020
 team_count <- 12
@@ -39,7 +30,6 @@ team_names <- players %>%
   distinct(fantasy_team, team_name)
 
 # Do first round of knapsacking -------------------------------------------
-
 players <- players %>% 
   select(-team_name) %>%
   group_by(fantasy_team) %>%
@@ -49,7 +39,6 @@ players <- players %>%
                         cap = salary_cap)) & !is.na(fantasy_team))
 
 # Find the available players ----------------------------------------------
-
 draft_values <- players %>% 
   ungroup() %>%
   filter(!knapsack) %>%
@@ -59,18 +48,14 @@ draft_values <- players %>%
   mutate(fantasy_team = rep_len(draft_order, length.out = n()),
          salary = 0)
 
-
 # Try to guess who would get drafted by who -------------------------------
-
 teams_with_picks <- players %>%
   bind_rows(draft_values) %>%
   group_by(fantasy_team) %>%
   top_n(30, WAR) %>% 
   arrange(fantasy_team)
 
-
 # Knapsack it again with draft pick guesses -------------------------------
-
 players_round2 <- teams_with_picks %>% 
   group_by(fantasy_team) %>%
   mutate(knapsack2 = as.logical(
@@ -78,22 +63,19 @@ players_round2 <- teams_with_picks %>%
                         profit = WAR, 
                         cap = salary_cap)))
 
-
-
 # Figure out keepers after second round -----------------------------------
 keepers <- players_round2 %>%
   filter(is.na(draft_pick), knapsack2) %>%
   left_join(team_names, by = "fantasy_team")
 
+
+# Make a summary table ----------------------------------------------------
 keepers_summary <- keepers %>%
   group_by(team_name, fantasy_team) %>%
   summarize(keepers = n(), total_salary = sum(salary)) %>%
   filter(!is.na(fantasy_team))
 
-
-
 # Write a pretty Excel worksheet via openxlsx ---------------------------------
-
 tabs <- split(keepers, keepers$team_name)
 tabs <- purrr::prepend(tabs, list(keepers_summary))
 names(tabs)[1] <- "Summary"
